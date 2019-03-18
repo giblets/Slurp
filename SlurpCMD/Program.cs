fb5b037b3bc544f24d63961f90845a3ecd91b0d7
Glenn Martin <glenn.e.martin@gmail.com>
2019-03-18 17:03:04 -0400

    Notes, and addition of Test Project

---
+++ SlurpCMD/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+
+namespace SlurpCMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+        }
+
+        public DataTable SlurpFileToDataTable(string fileNameWithPath)
+        {
+
+            DataTable dt = new DataTable("SlurpFile");
+            dt.Columns.Add("text", typeof(string));
+
+            string line;
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(fileNameWithPath);
+            while ((line = file.ReadLine()) != null)
+            {
+                dt.Rows.Add(line);
+            }
+            file.Close();
+
+            return dt;
+        }
+    }
+}
